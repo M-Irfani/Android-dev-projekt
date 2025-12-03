8b513e6dbcda0dc348059f6ecab99488ee6d852f
Mushtaq Irfani <mushtaqirfani10@gmail.com>
2025-12-03 16:18:38 +0100

    style: Update color palette for the app theme

--- app/src/main/java/mushtaq/projekt/gui/theme/Color.kt
+++ app/src/main/java/mushtaq/projekt/gui/theme/Color.kt
@@ -2,10 +2,13 @@ package mushtaq.projekt.gui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val FocusGreenPrimary = Color(0xFF2E7D32)
+val FocusGreenSecondary = Color(0xFF4CAF50)
+val FocusBackground = Color(0xFFF1F8E9)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = FocusGreenPrimary
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = FocusGreenSecondary
